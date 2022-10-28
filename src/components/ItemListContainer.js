1a486202ac28b70631fce2ad36b67cd6dd029a7c
hebernunez <hebernunez820@gmail.com>
2022-10-28 00:57:28 -0300

    Initial commit

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const ItemListContainer = () => {
+  return (
+    <div className="App">
+    
+        <p>
+          Proximante todos los productos necesarios para tu mascota!!!
+        </p>
+
+    </div>
+  )
+}
+
+
+
+export default ItemListContainer;
